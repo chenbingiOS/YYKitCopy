3b82d16e6ffb3bb2a640cfcbbe582e43a7722e7d
chen <bing.chen@dtise.com>
2017-07-12 07:41:47 +0800

    1 带属性文字各种的集中实现方法

---
+++ YYKitCopy/YYKitDemo/Text/YYTextExampleVC.h
@@ -0,0 +1,13 @@
+//
+//  YYTextExampleVC.h
+//  YYKitCopy
+//
+//  Created by 陈冰 on 2017/7/11.
+//  Copyright © 2017年 DTise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YYTextExampleVC : UITableViewController
+
+@end
