3fdaa0d2b56244ef09d6022a05c950b35c4fcdde
陈冰 <bing.chen@dtise.com>
2017-07-11 10:39:39 +0800

    1 YYKitCopy 项目初始化

---
+++ YYKitCopy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYKitCopy
+//
+//  Created by  chenbing on 2017/7/11.
+//  Copyright © 2017年 DTise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYKitCopy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYKitCopy
+//
+//  Created by  chenbing on 2017/7/11.
+//  Copyright © 2017年 DTise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
